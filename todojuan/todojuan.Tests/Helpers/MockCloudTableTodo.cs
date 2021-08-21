238f61a9d877d6c705c172df1b5b144195ce0400
estebanolaya25 <estebanolaya25@gmail.com>
2021-08-20 21:50:29 -0500

    Unit Test Preparation

---
+++ todojuan/todojuan.Tests/Helpers/MockCloudTableTodo.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todojuan.Tests.Helpers
+{
+    public class MockCloudTableTodo : CloudTable
+    {
+        public MockCloudTableTodo(Uri tableAdress) : base(tableAdress)
+        {
+        }
+
+        public MockCloudTableTodo(Uri tableAdress, StorageCredentials credentials) : base(tableAdress,credentials)
+        {
+        }
+
+        public MockCloudTableTodo(StorageUri tableAdress, StorageCredentials credentials) : base(tableAdress,credentials)
+        {
+        }
+
+        public override async Task<TableResult> ExecuteAsync(TableOperation operation)
+        {
+            return await Task.FromResult(new TableResult
+            {
+                HttpStatusCode = 200,
+                Result = TestFactory.GetTodoEntity()
+
+            }); 
+        }
+
+
+
+    }
+}
